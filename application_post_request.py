72aab3d42eba81497818148c5c28e72f556fdb59
tungxd <hoszetung2@gmail.com>
2026-09-07 14:20:59 +0800

    Add files via upload

---
+++ application_post_request.py
@@ -0,0 +1,23 @@
+import requests
+
+url = "http://career.wemine.hk/cv-submit"
+
+payload = {
+    "name": "Ho Sze Tung",
+    "email": "hoszetung2@gmail.com",
+    "position": "Web Developer Trainee",
+    "cv_url": "https://go.fliplink.me/view/3A0A909A-C459-4C47-BA33-6BCC9BBC3BB9",
+    "intro": "Hi! I am Ho Sze Tung, a motivated Information Technology graduate with hands-on experience in full-stack web development and database management. I am eager to use my technical skills to deliver reliable software solutions as a programmer.",
+    "website": "https://github.com/tungxd/HoSzeTung_Portfolio",
+    "note": "I look forward to hearing from you. Thank you!",
+    "code_url": "https://drive.google.com/file/d/1RIxsyS4lcshW4kUdTBXkymVMbbm_PFuP/view?usp=sharing"
+}
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print("Status Code:", response.status_code)
+print("Response Body:", response.json())
